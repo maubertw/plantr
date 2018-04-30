c021049be81eec1abf2dd908dd5f0913bf249ad2
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-04-30 16:18:52 -0500

    lamine

--- models.js
+++ models.js
@@ -18,6 +18,24 @@ const Gardener = db.define('gardener', {
 
 
 
+const Plot = db.define('plot', {
+  size : Sequelize.INTEGER,
+  shaded : Sequelize.BOOLEAN
+})
 
+
+const Vegetable = db.define('vegetable', {
+  name : Sequelize.STRING,
+  color : Sequelize.STRING,
+  planted_on:Sequelize.DATE
+})
+
+// Gardener.belongsTo(Plot)
+Plot.belongsTo(Gardener)
+
+Vegetable.belongsToMany(Plot,{through:"vegetable_plot"})
+Plot.belongsToMany(Vegetable,{through:"vegetable_plot"})
+
+Gardener.belongsTo(Vegetable,{as: "favorite_vegetable"} )
 
 module.exports = db;
